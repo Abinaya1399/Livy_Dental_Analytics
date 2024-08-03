c320225b59561ad744ee0558fa466d63262cee31
Abinaya Natarajan <haiabiraj@gmail.com>
2024-08-03 12:21:01 -0400

    Feat: Alert user with date range for duplicate records during file processing

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import hashlib
 import pandas as pd
 import mysql.connector
 
-
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 app.config['SESSION_TYPE'] = 'filesystem'
@@ -79,9 +78,6 @@ def process_files():
 
     return render_template('process_files.html', files_exist=files_exist)
 
-
-from flask import flash
-
 @app.route('/confirm_process', methods=['POST'])
 def confirm_process():
     if not session.get('logged_in'):
@@ -89,27 +85,31 @@ def confirm_process():
     try:
         files_processed = []
         duplicate_files = []
+        duplicate_summary = {}
         for filename in os.listdir(PROCESS_FOLDER):
             if filename.endswith('.xlsx') or filename.endswith('.csv'):
                 file_path = os.path.join(PROCESS_FOLDER, filename)
-                # account_type_key = filename.split('_')[0]
-                # mapped_account_type = ACCOUNT_TYPE_MAP.get(account_type_key, account_type_key)
                 for key in ACCOUNT_TYPE_MAP.keys():
                     if key in filename:
                         mapped_account_type = ACCOUNT_TYPE_MAP[key]
                         break
                 else:
                     mapped_account_type = "Unknown"
-                # date_str = filename.split('_')[-1].split('.')[0]
-                # year = date_str[:4]
 
-
-                status = process_file(file_path, mapped_account_type)
+                status, duplicates = process_file(file_path, mapped_account_type)
                 if status == "success":
                     files_processed.append(filename)
                 elif status == "duplicate":
                     duplicate_files.append(filename)
-
+                if duplicates:
+                    for account, date_range in duplicates.items():
+                        if account in duplicate_summary:
+                            existing_min_date, existing_max_date = duplicate_summary[account]
+                            new_min_date = min(existing_min_date, date_range[0])
+                            new_max_date = max(existing_max_date, date_range[1])
+                            duplicate_summary[account] = (new_min_date, new_max_date)
+                        else:
+                            duplicate_summary[account] = date_range
 
         if files_processed:
             flash_message = "<table class='table table-striped'><thead><tr><th>Processed Files</th></tr></thead><tbody>"
@@ -118,7 +118,6 @@ def confirm_process():
             flash_message += "</tbody></table>"
             flash(flash_message, 'info')
 
-
         if duplicate_files:
             duplicate_message = "<table class='table table-warning'><thead><tr><th>Duplicate Files</th></tr></thead><tbody>"
             for file in duplicate_files:
@@ -126,6 +125,12 @@ def confirm_process():
             duplicate_message += "</tbody></table>"
             flash(duplicate_message, 'warning')
 
+        if duplicate_summary:
+            duplicate_summary_message = "<div class='alert alert-warning' role='alert'><strong>Duplicate Records Found:</strong><ul>"
+            for account, (min_date, max_date) in duplicate_summary.items():
+                duplicate_summary_message += f"<li>Account {account}: From {min_date} to {max_date}</li>"
+            duplicate_summary_message += "</ul></div>"
+            flash(duplicate_summary_message, 'warning')
 
         if not files_processed and not duplicate_files:
             flash("No files to process!", 'info')
@@ -136,26 +141,24 @@ def confirm_process():
         flash(f"Error processing files: {e}", 'danger')
         return redirect(url_for('process_files'))
 
+
 def process_file(file_path, account_type):
     try:
         file_hash_key = generate_file_hash_key(file_path)
         file_name = os.path.basename(file_path)
-
 
         conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor()
         cursor.execute("SELECT COUNT(*) FROM file_hashes WHERE hash_key = %s", (file_hash_key,))
         if cursor.fetchone()[0] > 0:
             logger.info(f"File {file_path} has already been processed. Skipping.")
-            return "duplicate"
-
+            return "duplicate", None
 
         cursor.execute(
             "INSERT INTO file_hashes (file_path, file_name, hash_key, account_type, processed_date) VALUES (%s, %s, %s, %s, NOW())",
             (file_path, file_name, file_hash_key, account_type)
         )
         conn.commit()
-
 
         original_df = pd.read_excel(file_path)
 
@@ -169,15 +172,16 @@ def process_file(file_path, account_type):
         year = original_df['Posting Date'].dt.year.mode()[0]
         year = int(year)
 
-        save_to_staging_table(original_df, account_type, year)
+        duplicates_summary = save_to_staging_table(original_df, account_type, year)
         create_reporting_table(account_type, year)
 
         cursor.close()
         conn.close()
-        return "success"
+        return "success", duplicates_summary
     except Exception as e:
         logger.error(f"Error processing file: {e}")
         raise
+
 
 def save_to_staging_table(df, account_type, year):
     try:
@@ -218,12 +222,13 @@ def save_to_staging_table(df, account_type, year):
             )
         """)
 
+        duplicate_ids = []
+        duplicate_dates = []
         for index, row in df.iterrows():
             row_hash_key = generate_row_hash_key(row)
-            
-
-            cursor.execute("SELECT COUNT(*) FROM staging_table WHERE row_hash = %s", (row_hash_key,))
-            if cursor.fetchone()[0] == 0:
+            cursor.execute("SELECT id, posting_date FROM staging_table WHERE row_hash = %s", (row_hash_key,))
+            existing_row = cursor.fetchone()
+            if existing_row is None:
                 cursor.execute(
                     """
                     INSERT INTO staging_table (details, posting_date, description, amount, type, balance, check_or_slip, account_type, year, row_hash)
@@ -231,14 +236,25 @@ def save_to_staging_table(df, account_type, year):
                     """,
                     (row['Details'], row['Posting Date'], row['Description'], row['Amount'], row['Type'], row['Balance'], row['Check or Slip #'], account_type, year, row_hash_key)
                 )
+            else:
+                duplicate_ids.append(existing_row[0])
+                duplicate_dates.append(existing_row[1])
 
         conn.commit()
         cursor.close()
         conn.close()
         logger.info("Data successfully saved to staging table.")
+
+        if duplicate_ids:
+            min_date = min(duplicate_dates).strftime('%B %Y')
+            max_date = max(duplicate_dates).strftime('%B %Y')
+            return {account_type: (min_date, max_date)}
+        return {}
     except Exception as e:
         logger.error(f"Error saving to staging table: {e}")
         raise
+
+
 
 def create_reporting_table(account_type, year):
     try:
@@ -292,7 +308,6 @@ def create_reporting_table(account_type, year):
                     logger.info(f"Duplicate record found for id {row['id']} in account {account_type}")
                     continue  # Skip duplicate entry and continue with the next record
 
-
         conn.commit()
         cursor.close()
         conn.close()
@@ -313,7 +328,6 @@ def assign_category_subcategory_keyword(description, conn):
         if keyword_normalized in description_normalized:
             return row['category'], row['sub_category']
     return None, None
-
 
 @app.route('/view_dashboard')
 def view_dashboard():
